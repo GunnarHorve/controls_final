321bdae16a0b140d335e6071dcbabf5424109d1a
Gunnar Horve <gunnarhorve@gmail.com>
2017-12-03 19:25:17 -0500

    shrunk file; removed bloat

--- src/pd_traj_control.py
+++ src/pd_traj_control.py
@@ -1,62 +1,23 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2013-2015, Rethink Robotics
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice,
-#    this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the Rethink Robotics nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 """
 Baxter RSDK Joint Torque Example: joint springs
 """
-
-import argparse
-
 import rospy
-
-from dynamic_reconfigure.server import (
-    Server,
-)
-from std_msgs.msg import (
-    Empty,
-)
-
+from dynamic_reconfigure.server import Server
+from std_msgs.msg import Empty
 import baxter_interface
-
-from baxter_examples.cfg import (
-    JointSpringsExampleConfig,
-)
+from baxter_examples.cfg import JointSpringsExampleConfig
 from baxter_interface import CHECK_VERSION
 
-
-class JointSprings(object):
+class JointController(object):
     """
-    Virtual Joint Springs class for torque example.
+    Virtual Joint Springs class for torque control.
 
     @param limb: limb on which to run joint springs example
     @param reconfig_server: dynamic reconfigure server
 
-    JointSprings class contains methods for the joint torque example allowing
+    JointController class contains methods for the joint torque example allowing
     moving the limb to a neutral location, entering torque mode, and attaching
     virtual springs.
     """
@@ -159,7 +120,7 @@ class JointSprings(object):
         """
         Switches out of joint torque mode to exit cleanly
         """
-        print("\nExiting example...")
+        print("\nExiting...")
         self._limb.exit_control_mode()
         if not self._init_state and self._rs.state().enabled:
             print("Disabling robot...")
@@ -167,36 +128,22 @@ class JointSprings(object):
 
 
 def main():
-    """RSDK Joint Torque Example: Joint Springs
+    """Much of this code is cited from Baxter's Joint Torque Example
 
     Moves the specified limb to a neutral location and enters
     torque control mode, attaching virtual springs (Hooke's Law)
     to each joint maintaining the start position.
-
-    Run this example on the specified limb and interact by
-    grabbing, pushing, and rotating each joint to feel the torques
-    applied that represent the virtual springs attached.
-    You can adjust the spring constant and damping coefficient
-    for each joint using dynamic_reconfigure.
     """
-    arg_fmt = argparse.RawDescriptionHelpFormatter
-    parser = argparse.ArgumentParser(formatter_class=arg_fmt,
-                                     description=main.__doc__)
-    parser.add_argument(
-        '-l', '--limb', dest='limb', required=True, choices=['left', 'right'],
-        help='limb on which to attach joint springs'
-    )
-    args = parser.parse_args(rospy.myargv()[1:])
 
     print("Initializing node... ")
-    rospy.init_node("rsdk_joint_torque_springs_%s" % (args.limb,))
+    rospy.init_node("rsdk_joint_torque_springs_left")
     dynamic_cfg_srv = Server(JointSpringsExampleConfig,
                              lambda config, level: config)
-    js = JointSprings(args.limb, dynamic_cfg_srv)
-    # register shutdown callback
-    rospy.on_shutdown(js.clean_shutdown)
-    js.move_to_neutral()
-    js.attach_springs()
+
+    js = JointController("left", dynamic_cfg_srv) # instatiation control object
+    rospy.on_shutdown(js.clean_shutdown)       # register shutdown callback
+    js.move_to_neutral()                       # start from neutral arm position
+    js.attach_springs()                        # turn on control law
 
 
 if __name__ == "__main__":
